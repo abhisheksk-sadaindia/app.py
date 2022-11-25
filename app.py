b6d175ab407d1b72c7da487164c79489d185c227
ABHISHEK SREEKUMAR <abhishek.sreekumar@sada.com>
2022-11-25 16:09:58 +0530

    Test2

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "  Hanan                               🇦🇷   Mansoor"
+    return "  Hanan                               🇦🇷   Mansoor!"
